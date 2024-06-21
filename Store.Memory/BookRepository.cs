f7c1bcc8b11326efcff690c71bb2a1ca5890c054
Ivan <i.protzai@gmail.com>
2024-06-21 18:15:31 +0300

    web

---
+++ Store.Memory/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new Book[]
+            {
+                new Book(1, "Programming on C#"),
+                new Book(2, "Programming on C++"),
+                new Book(3, "Art of Programming"),
+                new Book(4, "Clean Code")
+            };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.ToLower().Contains(titlePart.ToLower())).ToArray();
+        }
+    }
+}
---
+++ domain/Store/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+        public Book(int Id, string Title)
+        {
+            this.Id = Id;
+            this.Title = Title;
+        }
+    }
+}
